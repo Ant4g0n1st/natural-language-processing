cfd8e698447772ea46b3d61b32eb2d8f2739a505
Ant4g0n1st <dragon124victor421force@gmail.com>
2019-02-21 21:10:46 -0600

    Stemming.

--- similar/similar.py
+++ similar/similar.py
@@ -4,7 +4,9 @@ import re
 
 name = input('Nombre del archivo : ')
 
-raw = BeautifulSoup(open(name).read(), 'html.parser').get_text()
+f = open(name)
+raw = BeautifulSoup(f.read(), 'html.parser').get_text()
+f.close()
 
 word_regex = re.compile(r'^\w+[\n]*$')
 digit_regex = re.compile(r'^\d+$')
@@ -15,4 +17,28 @@ tokens = list(filter(lambda x : word_regex.match(x) != None, tokens))
 
 tokens = list(filter(lambda x : digit_regex.match(x) == None, tokens))
 
-print(tokens)
+f = open('stop.txt')
+stop = set(f.read().split())
+f.close()
+
+tokens = list(filter(lambda x : x not in stop, tokens))
+
+stemMap = dict()
+
+f = open('stems.txt')
+for line in f:  
+    line = line.split()
+    stemMap[line[0]] = line[-1]    
+f.close()
+
+tokens = [stemMap.get(x, x) for x in tokens]
+
+f = open('processed.txt', 'w')
+f.write(" ".join(tokens))
+f.close()
+
+text = nltk.Text(tokens)
+
+freq = nltk.FreqDist(text)
+
+freq.plot(25)
