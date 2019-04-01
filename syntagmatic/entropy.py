1504382cbdaee42ca3045cc7f75478ec4629b54b
Ant4g0n1st <dragon124victor421force@gmail.com>
2019-04-01 00:55:51 -0600

    Entropy and MI syntagmatic relations.

--- syntagmatic/entropy.py
+++ syntagmatic/entropy.py
@@ -51,69 +51,74 @@ def LoadSpanishTagger():
 name = input('Nombre del archivo : ')
 raw = ReadTextFromHTML(name)
 
-stop = LoadStopwords()
-stemMap = LoadStems()
-
-# To train the spanish POS tagger.
-# tagger = nltk.UnigramTagger(nltk.corpus.cess_esp.tagged_sents())
-
-#sent_tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
+sent_tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
 
 word_regex = re.compile(r'^\w+[\n]*$')
 digit_regex = re.compile(r'\d+')
 
-#tagger = LoadSpanishTagger()
+tagger = LoadSpanishTagger()
+stop = LoadStopwords()
+stemMap = LoadStems()
 
-#sentences = []
-tokens = nltk.word_tokenize(raw)
-nouns = set()
-
-#sentTokens = list(map(lambda x : x.lower(), nltk.word_tokenize(sentence)))
-
-tokens = list(filter(lambda x : word_regex.match(x) != None, tokens))
-
-tokens = list(filter(lambda x : digit_regex.match(x) == None, tokens))
-
-tokens = list(filter(lambda x : x not in stop, tokens))
-
-tokens = [stemMap.get(x, x) for x in tokens]
+tokens = [ w.lower() for w in nltk.word_tokenize(raw) ]
+tokens = list(filter(lambda w : word_regex.match(w) != None, tokens))
+tokens = list(filter(lambda w : digit_regex.match(w) == None, tokens))
+tokens = list(filter(lambda w : w not in stop, tokens))
+tokens = [ stemMap.get(w, w) for w in tokens ]
 
 vocabulary = sorted(set(tokens))
 n = len(vocabulary)
 
+context = [ [ 0 for y in range(0, n) ] for x in range(0, n) ]
+contextCount = [ 0 for x in range(0, n) ]
+p = [ 0 for x in range(0, n) ]
 index = dict()
+m = 0
 
 for k in range(0, n):
     index[vocabulary[k]] = k
 
-context = [[0 for y in range(0, n)] for x in range(0, n)]
+sentences = sent_tokenizer.tokenize(raw)
+m = len(sentences)
 
-#nouns = sorted([index[x] for x in nouns])
-contextCount = [0 for x in range(0, n)]
-indices = [index[x] for x in tokens]
-p = [0 for x in range(0, n)]
+for sentence in sentences: 
 
-m = len(indices)
-w = 4 # For window-8
+    tokens = list(map(lambda w : w.lower(), nltk.word_tokenize(sentence)))
+    tokens = list(filter(lambda w : word_regex.match(w) != None, tokens))
+    tokens = list(filter(lambda w : digit_regex.match(w) == None, tokens))
+    tokens = list(filter(lambda w : w not in stop, tokens))
+    tokens = set(stemMap.get(w, w) for w in tokens)
 
-for k in range(0, m):
-    for l in range(1, w + 1):
-        if k - l > 0:
-            context[indices[k]][indices[k - l]] += 1
-            contextCount[indices[k - l]] += 1
-        if k + l < m:
-            context[indices[k]][indices[k + l]] += 1
-            contextCount[indices[k + l]] += 1
-    p[indices[k]] += 1
+    indices = [ index[w] for w in tokens if w in index ]
+    z = len(indices)
 
-#idf = [math.log((m + 1) / x) for x in contextCount]
+    for k in range(0, z):
+        context[indices[k]][indices[k]] += 1
+        contextCount[indices[k]] += 1
+        p[indices[k]] += 1
+        for l in range(k + 1, z):
+            context[indices[k]][indices[l]] += 1
+            context[indices[l]][indices[k]] += 1
 
-p = list(map(lambda x : x / m, p))
+idf = [ math.log((m + 1) / x) for x in contextCount ]
 
 for k in range(0, n):
-    s = sum(context[k]) 
+    p[k] = math.log1p(p[k]) * idf[k]
+
+total = sum(p)
+
+p = list(map(lambda x : x / total, p))
+
+total = 0
+for k in range(0, n):
     for l in range(0, n):
-        context[k][l] = context[k][l] / s
+        context[k][l] = math.log1p(context[k][l]) * idf[l]
+    total += sum(context[k])
+
+for k in range(0, n):
+    for l in range(0, n):
+        context[k][l] /= total
+        #context[k][l] *= idf[l] 
         #context[k][l] = math.log1p(context[k][l]) * idf[l]
 
 while True:
@@ -127,31 +132,59 @@ while True:
     x = index[word]
     h = []
 
+    px, npx = p[x], 1 - p[x]
     for k in range(0, n):
-        px, npx = p[x], 1 - p[x]
         pk, npk = p[k], 1 - p[k]
-        p11 = context[x][k]
-        p10 = pk - p11
-        p01 = px - p11
-        p00 = npk - p01 
+        p11 = context[k][x]
+        p10 = px - p11
+        p01 = pk - p11
+        p00 = p11 + p10 + p01
+        p00 = 1 - p00
         h0 = 0
         if p00 > 0:
             h0 -= p00 * math.log2(p00)
         if p10 > 0:
             h0 -= p10 * math.log2(p10)
-        h0 *= npx
         h1 = 0
         if p01 > 0:
             h1 -= p01 * math.log2(p01)
         if p11 > 0:
             h1 -= p11 * math.log2(p11)
-        h1 *= px
-        h.append((h0 + h1, k))
+        h.append((h0 * npk + h1 * pk, k))
 
-    h = sorted(h)
+    h = sorted(h, reverse = True)
 
-    f = open("syntagmatic-to-" + word + ".txt", 'w')
+    f = open(word + "-entropy.txt", 'w')
     for (x, y) in h:
+        f.write(vocabulary[y] + " \t " + str(x) + "\n")
+    f.close()
+    
+    x = index[word]
+    i = []
+
+    px, npx = p[x], 1 - p[x]
+    for k in range(0, n):
+        pk, npk = p[k], 1 - p[k]
+        p11 = context[k][x]
+        p10 = px - p11
+        p01 = pk - p11
+        p00 = p11 + p10 + p01
+        p00 = 1 - p00
+        ik = 0
+        if p00 > 0:
+            ik += p00 * math.log2(p00 / (npx * npk))
+        if p01 > 0:
+            ik += p01 * math.log2(p01 / (npx * pk))
+        if p10 > 0:
+            ik += p10 * math.log2(p10 / (px * npk))
+        if p11 > 0:
+            ik += p11 * math.log2(p11 / (px * pk))
+        i.append((ik, k))
+
+    i = sorted(i, reverse = True)
+
+    f = open(word + "-mi.txt", 'w')
+    for (x, y) in i:
         f.write(vocabulary[y] + " \t " + str(x) + "\n")
     f.close()
 
